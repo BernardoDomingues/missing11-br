57ad4e6a8f78bbfef503301aef43a20e1d20b370
Bernardo Domingues <bernardoopd@hotmail.com>
2022-04-02 13:02:05 -0300

    corrige bug de tentativas

--- src/Player.js
+++ src/Player.js
@@ -6,9 +6,10 @@ import Modal from "./Modal";
 const Player = ({ players, playerData, handleData }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [inputData, setInputData] = useState('');
-  const newPlayerData = playerData;
+  const [attemptsLeft, setAttemptsLeft] = useState(playerData.attemptsLeft);
 
   const checkAttempt = () => {
+    const newPlayerData = playerData;
     const newData = players;
     if (inputData.toLowerCase() === playerData.name) {
       newPlayerData.hidden = false;
@@ -19,20 +20,21 @@ const Player = ({ players, playerData, handleData }) => {
       newPlayerData.attemptsLeft = playerData.attemptsLeft - 1;
       newData[playerData.id] = newPlayerData;
       handleData(newData);
+      setAttemptsLeft(attemptsLeft - 1);
     }
   };
 
-  const validadeButton = () => newPlayerData.attemptsLeft < 1 || newPlayerData.hidden === false ? true : false
+  const validadeButton = () => attemptsLeft < 1 || playerData.hidden === false ? true : false
 
   return (
     <>
-    <Container disabled={validadeButton()} onClick={() => setIsModalOpen(true)}>{newPlayerData.name.length}</Container>
-    {newPlayerData.hidden ? <HiddenPlayerName>{newPlayerData.name}</HiddenPlayerName> : <PlayerName>{newPlayerData.name}</PlayerName>}
-    <PlayerPosition>{newPlayerData.position}</PlayerPosition>
+    <Container disabled={validadeButton()} onClick={() => setIsModalOpen(true)}>{playerData.name.length}</Container>
+    {playerData.hidden ? <HiddenPlayerName>{playerData.name}</HiddenPlayerName> : <PlayerName>{playerData.name}</PlayerName>}
+    <PlayerPosition>{playerData.position}</PlayerPosition>
     <Modal visible={isModalOpen} onClose={() => setIsModalOpen(false)}>
       <BodyModal>
         <div>
-          Tentativas Restantes: {newPlayerData.attemptsLeft}
+          Tentativas Restantes: {attemptsLeft}
         </div>
         <input type="text" onChange={(event) => setInputData(event.target.value)}/>
         <div>
